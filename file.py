afb256dc8b666a24d83db700b28cf6315c51a54b
wzbury12 <105519376+wzbury12@users.noreply.github.com>
2022-05-21 16:01:48 +0200

    2nd function

--- file.py
+++ file.py
@@ -6,7 +6,26 @@ def greeter(func):
 
 
 def sums_of_str_elements_are_equal(func):
-    pass
+    def wrapper(*args, **kwargs):
+        tab = func(*args, **kwargs).split()
+        sumx = 0
+        sumy = 0
+        for x in tab[0]:
+            if(x != "-"):
+                sumx += int(x)
+        if (tab[0][0]=="-"):
+            sumx *= -1
+        for x in tab[1]:
+            if(x != "-"):
+                sumy += int(x)
+        if (tab[1][0]=="-"):
+            sumy *= -1
+        
+        if (sumx == sumy):
+            return str(sumx) + " == " + str(sumy)
+        else:
+            return str(sumx) + " != " + str(sumy)
+    return wrapper
 
 
 def format_output(*required_keys):
